5b391b1e065656c70d1f6fc099c6ec0459067947
FernandoVS11 <a18000997@alumnos.uady.mx>
2025-08-02 10:42:24 -0600

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,28 +2,17 @@ module.exports = {
   env: {
     browser: true,
     node: true,
-    es2021: true,
-    'cypress/globals': true
+    es2021: true
   },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:cypress/recommended'
+    'plugin:react/recommended'
   ],
-  plugins: ['cypress'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
   rules: {
     quotes: ['error', 'single']
-  },
-  overrides: [
-    {
-      files: ['e2e-tests/**/*.js'],
-      env: {
-        'cypress/globals': true
-      }
-    }
-  ]
+  }
 }
