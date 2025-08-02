e0f577910a4d1003ce437a5a8ca280f535219ff0
FernandoVS11 <a18000997@alumnos.uady.mx>
2025-08-02 10:56:02 -0600

    fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,29 @@ module.exports = {
   env: {
     browser: true,
     node: true,
-    es2021: true
+    es2021: true,
+    jest: true, // ✅ esto es lo nuevo
+    'cypress/globals': true
   },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended'
+    'plugin:react/recommended',
+    'plugin:cypress/recommended'
   ],
+  plugins: ['cypress'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
   rules: {
     quotes: ['error', 'single']
-  }
-}
+  },
+  overrides: [
+    {
+      files: ['e2e-tests/**/*.js'],
+      env: {
+        'cypress/globals': true
+      }
+    }
+  ]
+};
--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom'
 
 const PokemonList = ({ pokemonList }) => {
@@ -16,5 +17,13 @@ const PokemonList = ({ pokemonList }) => {
     </div>
   )
 }
+PokemonList.propTypes = {
+  pokemonList: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired
+    })
+  ).isRequired
+};
 
 export default PokemonList
